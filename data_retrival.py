4c7355a9baf34999c8dc4b432279195a0f8e0467
Mattia Martino <m25martino@gmail.com>
2025-02-11 15:16:41 +0100

    initial commit

---
+++ data_retrival.py
@@ -0,0 +1,35 @@
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+
+import os
+
+API_KEY = 'PK9E85AK8QRJVNZC7UYZ'
+SECRET_KEY = 'T6uSv4lkOPvlMBKS1Q5OGGWdbaCtuaLR62MzLmpq'
+
+def initialize_client():
+    return StockHistoricalDataClient(API_KEY, SECRET_KEY)
+
+def check_dir(path_dir:str) -> None:
+    os.makedirs(path_dir, exist_ok=True)
+
+def save_bar_data(symbol:str, timeframe, start:str, end:str, save_dir:str) -> None:
+
+    check_dir(save_dir)
+
+    client = initialize_client()
+    request = StockBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=timeframe,
+        start=start, 
+        end=end)
+    
+    print(f"\nRetrieving data for: {symbol} from {start} to {end}...")
+    bars = client.get_stock_bars(request)
+    bars = bars.df
+
+    bars.to_csv(f'./{save_dir}/{symbol}_{timeframe}_{start}_{end}.csv')
+    print(f"Data saved to: {save_dir}/{symbol}_{timeframe}_{start}_{end}.csv\n")
+
+
+save_bar_data('NVDA', TimeFrame.Minute, '2023-08-01', '2024-01-31', 'data')
